81e4ba902c0e4593edf781456a20f8e39e832673
debugerliu <719918768@qq.com>
2020-03-10 16:46:48 +0800

    第一次上传

---
+++ TWO/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from TWO import views
+
+urlpatterns = [
+    url('index/', views.index),
+    url('add_student/', views.add_student),
+    url('get_student/', views.get_student),
+    url('update_student/', views.update_student)
+]
---
+++ TWO/views.py
@@ -0,0 +1,40 @@
+import random
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from TWO.models import Student
+
+
+def index(request):
+    return HttpResponse(123666)
+
+
+def add_student(request):
+    student = Student()
+    student.s_name = 'Tom%d' % random.randrange(100)
+    student.s_age = random.randrange(100)
+    student.save()
+    return HttpResponse('添加成功 %s' % student.s_name)
+
+
+def get_student(request):
+    students = Student.objects.all()
+    for i in students:
+        print(i)
+
+    # return HttpResponse('查询成功')
+    context = {
+        'hobby': 'game',
+        'eat': 'meat',
+        'students': students
+    }
+    return render(request, 'student_list.html', context=context)
+
+
+def update_student(request):
+    student = Student.objects.get(pk=3)
+    student.s_name = 'Tom000'
+    student.save()
+    return HttpResponse('更新成功')
